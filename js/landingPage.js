eb588beb7800f9522927b0622ae1c700a208afac
CoryF <coryf65@gmail.com>
2020-04-12 16:34:48 -0500

    finished landing page. added some moving clouds for fun.

--- js/landingPage.js
+++ js/landingPage.js
@@ -1,11 +1,44 @@
 // This JS will handle the fun landing page animations using JQuery
 
-$(document).ready(function(){
-    $("button").click(function(){
-      var div = $("div");
-      div.animate({height: '300px', opacity: '0.4'}, "slow");
-      div.animate({width: '300px', opacity: '0.8'}, "slow");
-      div.animate({height: '100px', opacity: '0.4'}, "slow");
-      div.animate({width: '100px', opacity: '0.8'}, "slow");
-    });
-  });
+function onLoad() {
+  AnimateCloud1();
+  AnimateCloud2();
+}
+
+function AnimateCloud1() {
+
+  const elem = document.getElementById("Cloud1");   
+  let pos = 0;  
+  let id = setInterval(frame, 10);  
+
+  function frame() {
+    let endOfWindow = window.innerWidth;
+    if (pos == endOfWindow) {
+      // condition to stop the animation
+      clearInterval(id);
+    } else {
+      pos++; 
+      elem.style.left = pos + "px"; 
+    }
+  }
+  
+}
+
+function AnimateCloud2() {
+
+  const elem = document.getElementById("Cloud2");   
+  let pos = 0;  
+  let id = setInterval(frame, 80);  
+
+  function frame() {
+    let endOfWindow = window.innerWidth;
+    if (pos == endOfWindow) {
+      // condition to stop the animation
+      clearInterval(id);
+    } else {
+      pos++; 
+      elem.style.left = pos + "px"; 
+    }
+  }
+  
+}
