96f3923980f510400ea6536ce9ff80eb2818c43a
CoryF <coryf65@gmail.com>
2020-05-04 16:29:19 -0500

    TODO work on wiggles for the letters

--- js/main.js
+++ js/main.js
@@ -1,16 +1,36 @@
 // Main JS
+$(document).ready(function() {
+    
+    $('span').hover(wiggleAnimation);
+
+    //animateCSS('.wiggle', 'bounce')
+
+});
+
+// TODO work on wiggles for the letters 
 
 // add css class on hover of certain class items ?
-$('.wiggle').mouseover(() => wiggleAnimation(this) );
+function wiggleAnimation() {
 
-function wiggleAnimation(element) {
+    console.log('wiggle');
+    const element =  document.querySelector('span');
+    void element.offsetWidth;
+    element.classList.add('animated', 'infinite', 'wobble');
+    void element.offsetWidth;
     
-    if ($('.wiggle').hasClass('shake').toString() = "shake") {
-        console.log($('.wiggle').hasClass('shake').toString())
-        console.log("adding class", $('.wiggle'))
-        $('.wiggle').removeClass("wiggle").addClass('shake');
-        console.log($('.wiggle').class().toString())
-    } else {
-        console.log('done')
+}
+
+function animateCSS(element, animationName, callback) {
+    console.log('animateCSS');
+    const node = document.querySelector(element);
+    node.classList.add('animated', animationName);
+
+    function handleAnimationEnd() {
+        node.classList.remove('animated', animationName);
+        node.removeEventListener('animationend', handleAnimationEnd);
+
+        if (typeof callback === 'function') callback();
     }
+
+    node.addEventListener('animationend', handleAnimationEnd);
 }
