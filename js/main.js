76b7a5b1f065e9445c85d77a6d4276f8f9b2d35b
CoryF <coryf65@gmail.com>
2020-09-10 21:37:13 -0500

    fixed animation, moved img to correct dir

--- js/main.js
+++ js/main.js
@@ -1,36 +1,11 @@
 // Main JS
 $(document).ready(function() {
     
-    $('span').hover(wiggleAnimation);
-
-    //animateCSS('.wiggle', 'bounce')
-
+    // wave the hand!    
+    $("#wave").hover(
+        //wave on enter
+        function() { $( this ).addClass("animate__animated animate__wobble")},        
+        // reset animation, would like a delay on removing
+        function() { $( this ).removeClass("animate__animated animate__wobble");}
+    );
 });
-
-// TODO work on wiggles for the letters 
-
-// add css class on hover of certain class items ?
-function wiggleAnimation() {
-
-    console.log('wiggle');
-    const element =  document.querySelector('span');
-    void element.offsetWidth;
-    element.classList.add('animated', 'infinite', 'wobble');
-    void element.offsetWidth;
-    
-}
-
-function animateCSS(element, animationName, callback) {
-    console.log('animateCSS');
-    const node = document.querySelector(element);
-    node.classList.add('animated', animationName);
-
-    function handleAnimationEnd() {
-        node.classList.remove('animated', animationName);
-        node.removeEventListener('animationend', handleAnimationEnd);
-
-        if (typeof callback === 'function') callback();
-    }
-
-    node.addEventListener('animationend', handleAnimationEnd);
-}
